c2daee5e3f7344ea920b0994d84d218bd6ae08a0
fergusKe <fergus.jingtai@gmail.com>
2018-03-13 01:16:47 +0800

    init

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import ImagePreview from '../../components/ImagePreview/ImagePreview';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <ImagePreview />
+      </div>
+    );
+  }
+}
+
+export default Home;
